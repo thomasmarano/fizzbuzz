dfe603a982dec43473d0c4d4b776fb1830238064
Thomas <thomas.marano2@gmail.com>
2018-12-21 09:15:37 +0000

    Finished FizzBuzz Pairing Exercise

--- lib/fizzbuzz.rb
+++ lib/fizzbuzz.rb
@@ -1,3 +1,15 @@
 def fizzbuzz(number)
-	'fizz'
+		if number == 0
+				0
+		elsif (number % 3 == 0 && number % 5 == 0)
+				'fizzbuzz'
+		elsif (number % 3 == 0)
+				'fizz'
+		elsif (number % 5 == 0)
+				'buzz'
+		elsif number.is_a?(Integer)
+				number
+		else
+				"Please enter an integer"
+		end
 end
--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -3,4 +3,19 @@ describe 'fizzbuzz' do
 	it 'returns "fizz" when passed 3' do
 		expect(fizzbuzz(3)).to eq 'fizz'
 	end
+	it 'returns "buzz" when passed 5' do
+		expect(fizzbuzz(5)).to eq 'buzz'
+	end
+	it 'returns "fizzbuzz" when passed 15' do
+		expect(fizzbuzz(15)).to eq 'fizzbuzz'
+	end
+	it 'returns 2 when passed 2' do
+		expect(fizzbuzz(2)).to eq 2
+	end
+	it 'returns 0 when passed 0' do
+		expect(fizzbuzz(0)).to eq 0
+	end
+	it 'returns "Error" when passed "a"' do
+		expect(fizzbuzz("a")).to eq "Please enter an integer"
+	end
 end
